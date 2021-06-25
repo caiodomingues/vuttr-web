8bf01234e54037279b828b49466e4e37ef78887c
Caio Domingues <caiodss99@hotmail.com>
2021-06-25 18:39:33 -0300

    fix: mistypo URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const api = Axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "https://vuttr-domingues.herokuapp.com/",
+  baseURL: "https://vuttr-domingues.herokuapp.com/api",
   headers: {
     Accept: "application/json",
     "Content-type": "application/json",
